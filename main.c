f870e765faa6ba8820fcfc98214bcdbe5d1b1d2a
21abhi <1abhijitthaware@gmail.com>
2019-11-22 15:16:53 +0530

    Arithmatic program is successfully created

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"arith.h"
+
+int main()
+{
+    
+	printf("Add is :%d\n",add(40,30));
+        printf("sub is %d\n",sub(10,5));
+        printf("multipliaction is %d\n",mul(10,10));
+  	printf("division is %d\n",div(10,5));
+	return 0;
+}
+
---
+++ sub.c
@@ -0,0 +1,6 @@
+int  sub(int a,int b)
+{ 
+   int s;
+   s=a-b;
+   return s;
+}
